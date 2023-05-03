6ede830b446deae4dc00ed001a9ebdd3f69f9a27
ljpson <ljpson@naver.com>
2023-05-03 10:44:21 +0900

    CSS수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Banner() {
     return (
-        <section className='h-96 bg-yellow-900 relative'>
+        <section className='h-48 bg-yellow-900 relative'>
             <div className='w-full h-full bg-cover bg-banner opacity-80'></div>
-            <div className='absolute w-full top-32 text-center text-gray-50 drop-shadow-2xl'>
-                <h2 className='text-6xl'>Shop with US</h2>
-                <p className='text-2xl'>Best Products, High Quality</p>
+            <div className='absolute w-full top-7 text-center text-gray-50 drop-shadow-2xl'>
+                <h2 className='text-5xl md:text-6xl'>Shop with US</h2>
+                <p className='text-lg md:text-2xl'>Best Products, High Quality</p>
             </div>
         </section>
     );
