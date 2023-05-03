9a0fb136e3daac4262ea68f1099ca93361cb56ef
ljpson <ljpson@naver.com>
2023-05-03 16:50:34 +0900

    CSS수정

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PriceCard({text, price}) {
     return (
-        <div className='bg-gray-50 p-2 sm:p-7 md:p-10 mx-1 rounded-2xl text-center text-base md:text-xl'>
+        <div className='bg-gray-50 p-3 sm:p-7 md:p-10 mx-1 rounded-2xl text-center text-base md:text-xl'>
             <p>{text}</p>
             <p className='font-bold text-brand'>{price}원</p>
         </div>
